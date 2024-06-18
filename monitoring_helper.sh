bf8408d57ac68f894e67eccc1e1b03c0f4d04bee
Carlos Jorge Pereira <144349074+podefteza@users.noreply.github.com>
2024-06-18 19:20:19 +0100

    Create monitoring_helper.sh

---
+++ monitoring_helper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Function to calculate minutes since boot
+calculate_minutes_since_boot() {
+    boot_time=$(who -b | awk '$1 == "system" {print $4 " " $5}')
+    boot_epoch=$(date -d "$boot_time" +%s)
+    current_epoch=$(date +%s)
+    minutes_since_boot=$(( (current_epoch - boot_epoch) / 60 ))
+    echo "$minutes_since_boot"
+}
+
+# Interval in minutes
+interval_minutes=10
+
+# Calculate minutes since boot
+minutes_since_boot=$(calculate_minutes_since_boot)
+
+# Check if the current time is a multiple of the interval since boot
+if [ $(( minutes_since_boot % interval_minutes )) -eq 0 ]; then
+    /usr/local/bin/monitoring.sh
+fi
